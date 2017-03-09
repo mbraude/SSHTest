65e9d4d46b82df556d5c03a1d3a3b6cf047a1b07
Michael Braude <mbraude@microsoft.com>
2017-03-08 17:06:21 -0800

    Adding console.log statement

--- SSHTest/server.js
+++ SSHTest/server.js
@@ -4,5 +4,6 @@ var port = 80;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+    console.log("Got a new request!");
 }).listen(port);
 //# sourceMappingURL=server.js.map
--- SSHTest/server.ts
+++ SSHTest/server.ts
@@ -4,4 +4,5 @@ var port = 80;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
+    console.log("Got a new request!");
 }).listen(port);
