5cf03c2695fe76012b44b49ebd32e0aaa38ca5e9
Michael Braude <mbraude@microsoft.com>
2017-03-09 23:45:54 -0800

    Adding the client

---
+++ SSHTest/Client.js
@@ -0,0 +1,13 @@
+"use strict";
+var net = require("net");
+var client = net.connect("\\\\.\\pipe\clouddebugger", function () {
+    console.log("Connected to Cloud Debugger");
+});
+// Relay all output from the pipe to our stdout:
+client.on("data", function (data) {
+    console.log(data.toString());
+});
+client.on('end', function () {
+    console.log("Cloud Debugger disconnected unexpectedly");
+});
+//# sourceMappingURL=client.js.map
---
+++ SSHTest/Client.ts
@@ -0,0 +1,14 @@
+﻿import net = require('net');
+
+var client = net.connect("\\\\.\\pipe\clouddebugger", () => {
+    console.log("Connected to Cloud Debugger");
+});
+
+// Relay all output from the pipe to our stdout:
+client.on("data", (data: Buffer) => {
+    console.log(data.toString());
+});
+
+client.on('end', function () {
+    console.log("Cloud Debugger disconnected unexpectedly");
+})
