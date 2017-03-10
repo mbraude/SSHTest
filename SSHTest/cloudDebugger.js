8202b4ce31d82106bc32225df8308d487a7c08b2
Michael Braude <mbraude@microsoft.com>
2017-03-10 08:45:11 -0800

    Trying stdout instead of console.log

--- SSHTest/cloudDebugger.js
+++ SSHTest/cloudDebugger.js
@@ -5,7 +5,8 @@ var net = require("net");
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-    console.log("Request Recieved!");
+    //console.log("Request Recieved!");
+    process.stdout.write("Request Recieved!");
 }).listen(80);
 var server = net.createServer(function (stream) {
     // pipe stdout to the stream:
--- SSHTest/cloudDebugger.ts
+++ SSHTest/cloudDebugger.ts
@@ -5,10 +5,11 @@ import net = require('net');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-    console.log("Request Recieved!");
+    //console.log("Request Recieved!");
+    process.stdout.write("Request Recieved!");
 }).listen(80);
 
-var server = net.createServer(function (stream) {
+var server = net.createServer((stream: net.Socket) => {
 
     // pipe stdout to the stream:
     process.stdout.on("data", (data: Buffer) => stream.write(data));
