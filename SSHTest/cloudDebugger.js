7205e95dd0b0af14f9c42d400759dc6869b90f26
Michael Braude <mbraude@microsoft.com>
2017-03-14 12:56:54 -0700

    Delete the named pipe before we start it again

--- SSHTest/cloudDebugger.js
+++ SSHTest/cloudDebugger.js
@@ -2,6 +2,8 @@
 /// <reference path="typings/index.d.ts" />
 var http = require("http");
 var net = require("net");
+var fs = require("fs");
+var namedPipeFileName = "\\\\.\\clouddebugger";
 var httpServer = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
@@ -41,6 +43,10 @@ process.stdin.on("data", function (data) {
 });
 // Listen on port 80 for the web server:
 httpServer.listen(80);
+// Delete the existing named pipe before we make a new one:
+if (fs.existsSync(namedPipeFileName)) {
+    fs.unlinkSync(namedPipeFileName);
+}
 // Listen on an IPC channel
-namedPipeServer.listen("\\\\.\\clouddebugger");
+namedPipeServer.listen(namedPipeFileName);
 //# sourceMappingURL=cloudDebugger.js.map
--- SSHTest/cloudDebugger.ts
+++ SSHTest/cloudDebugger.ts
@@ -1,6 +1,9 @@
 ﻿/// <reference path="typings/index.d.ts" />
 import http = require('http');
 import net = require('net');
+import fs = require('fs');
+
+var namedPipeFileName = "\\\\.\\clouddebugger";
 
 var httpServer = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -54,5 +57,10 @@ process.stdin.on("data", (data: Buffer) => {
 // Listen on port 80 for the web server:
 httpServer.listen(80);
 
+// Delete the existing named pipe before we make a new one:
+if (fs.existsSync(namedPipeFileName)) {
+    fs.unlinkSync(namedPipeFileName);
+}
+
 // Listen on an IPC channel
-namedPipeServer.listen("\\\\.\\clouddebugger");
+namedPipeServer.listen(namedPipeFileName);
